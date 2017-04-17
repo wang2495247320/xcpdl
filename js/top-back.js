1d568ee0d5a2fc1484386805c3affc61ba1dd4a0
wangbiaomeng <2495247320@qq.com>
2017-04-17 10:01:37 +0800

    Signed-off-by: wangbiaomeng <2495247320@qq.com>

---
+++ js/top-back.js
@@ -0,0 +1,36 @@
+// ����ͼ�� �Ѽ����� www.lanrentuku.com
+function WeixinTop(){
+	this.init();
+}
+WeixinTop.prototype = {
+	constructor: WeixinTop,
+	init: function(){		
+		this._initBackTop();
+	},	
+	_initBackTop: function(){
+		var $backTop = this.$backTop = $('<div class="cbbfixed">'+
+						
+						'<a class="gotop cbbtn">'+
+							'<span class="up-icon"></span>'+
+						'</a>'+
+					'</div>');
+		$('body').append($backTop);
+		
+		$backTop.click(function(){
+			$("html, body").animate({
+				scrollTop: 0
+			}, 120);
+		});
+		var timmer = null;
+		$(window).bind("scroll",function() {
+            var d = $(document).scrollTop(),
+            e = $(window).height();
+            0 < d ? $backTop.css("bottom", "20px") : $backTop.css("bottom", "-90px");
+			clearTimeout(timmer);
+			timmer = setTimeout(function() {
+                clearTimeout(timmer)
+            },100);
+	   });
+	}
+}
+var WeixinTop = new WeixinTop();
